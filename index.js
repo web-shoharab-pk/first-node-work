3ff332cca12165c2a12078ff8ab2ca23f45b8a19
md shoharab pk <shoharabpk.2019@gmail.com>
2021-03-24 10:33:09 +0600

    node express simple complete

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 
-function rootCall(req, res) {
-   res.send("thanks") 
-}
+//cors
+app.use(cors())
 
-app.get('./', (req, res) =>{
+//body_parser
+app.use(bodyParser.json())
+
+ 
+// get method
+app.get('/', (req, res) =>{
     res.send("thanks for calling me")
 } )
+ 
+const users = ['shoharab', 'shohan', 'shohel', 'alamin', 'nirob', 'rabby', 'roky']
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+     const name = users[userId]
+     console.log(name);
+     res.send({userId, name})
+})
+
+// post method
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    user.id = 21
+    // save to database
+    res.send(user )
+    console.log('data recive', user);
+})
 
 app.listen(4000, () => console.log("listening port 4000"))
